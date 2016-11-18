781f6cef322654ef759af3ee08e2af76f0d7f21c
778604 <778604@XATLCAMP01434.delta.rl.delta.com>
2016-11-18 05:08:56 -0500

    added one more comment

--- vs-springboot-starter/src/main/java/com/vs/VSBoot.java
+++ vs-springboot-starter/src/main/java/com/vs/VSBoot.java
@@ -3,6 +3,7 @@ package com.vs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Spring boot application
+//Added local branch
 @SpringBootApplication
 public class VSBoot
 {
